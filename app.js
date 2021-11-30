03fd7fef7dc7dfd0649b7b891f4a684fc7acc61e
Cos <constantiin@bk.ru>
2021-11-30 22:55:29 +0300

    update gallery plugin

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const slides = document.querySelectorAll('.slide');
+function galleryPlugin(activeSlide = 2) {
+    const slides = document.querySelectorAll('.slide');
 
-function clearActiveSlide () {
+    slides[activeSlide].classList.add('active');
+
+    function clearActiveSlide () {
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+        });
+    };
+
     slides.forEach((slide) => {
-        slide.classList.remove('active');
+        slide.addEventListener('click', () => {
+            clearActiveSlide();
+
+            slide.classList.add('active');
+        });
     });
 };
 
-slides.forEach((slide) => {
-    slide.addEventListener('click', () => {
-        clearActiveSlide();
-
-        slide.classList.add('active');
-    });
-});
+galleryPlugin();
